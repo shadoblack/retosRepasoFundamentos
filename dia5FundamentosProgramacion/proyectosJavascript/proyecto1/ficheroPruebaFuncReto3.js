3fb4edb064b83d15eca49d5a2024463b043b4b12
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-01-21 11:04:55 +0100

    Delete dia5FundamentosProgramacion directory

--- dia5FundamentosProgramacion/proyectosJavascript/proyecto1/ficheroPruebaFuncReto3.js
+++
@@ -1,7 +0,0 @@
-let myLib = require("C:\\Users\\edgar\\Desktop\\proyectosBootcamp\\proyectoscodenotch\\dia5FundamentosProgramacion\\proyectosJavascript\\proyecto1\\libreriaVector");
-
-console.log(myLib.crearVector(10,50));
-console.log(myLib.sumaVector(myLib.crearVector(10,50),myLib.crearVector(10,50)));
-console.log(myLib.productoNumeroVector(10,myLib.crearVector(10,50)));
-console.log(myLib.restaVector(myLib.crearVector(10,50),myLib.crearVector(10,50)));
-console.log(myLib.productoVector(myLib.crearVector(10,50),myLib.crearVector(10,50)));
--- dia5FundamentosProgramacion/proyectosJavascript/proyecto1/index.js
+++
@@ -1,5 +0,0 @@
-let suma = (op1,op2) => {return op1+op2};
-let resta = (op1,op2) => {return op1-op2};
-let multi = (op1,op2) => {return op1*op2};
-let divi = (op1,op2) => {return op1/op2};
-module.exports ={suma,resta,multi,divi};
